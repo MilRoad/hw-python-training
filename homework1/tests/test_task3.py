5aa6d3b9178ff8ac1990a7f56180bb0baf03c34d
MilRoad <milena_laistseva@mail.ru>
2020-11-07 15:01:33 +0300

    change paths to test text files and add init file

--- homework1/tests/test_task3.py
+++ homework1/tests/test_task3.py
@@ -1,3 +1,4 @@
+import os
 from typing import Tuple
 
 import pytest
@@ -8,8 +9,8 @@ from homework1.tasks.task3 import find_maximum_and_minimum
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        ("files/test1.txt", (1, 8)),
-        ("files/test2.txt", (-1, 97)),
+        (os.path.abspath(os.path.dirname(__file__)) + "/files/test1.txt", (1, 8)),
+        (os.path.abspath(os.path.dirname(__file__)) + "/files/test2.txt", (-1, 97)),
     ],
 )
 def test_find_maximum_and_minimum(value: str, expected_result: Tuple[int, int]):
